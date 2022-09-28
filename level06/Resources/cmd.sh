f5c77cad1f37e0b2a82937c5601060569610a3e0
mhufflep <mhufflep@student.21-school.ru>
2022-09-29 04:05:31 +0500

    level06 updated

--- level06/Resources/cmd.sh
+++ level06/Resources/cmd.sh
@@ -1,3 +1,8 @@
+# Curly syntax
+# https://www.php.net/manual/en/language.types.string.php#language.types.string.parsing.complex
+
+# What \\1 means
+# https://stackoverflow.com/questions/54954183/what-does-this-1-mean-in-php
 
 # About /e pattern modifier
 # https://stackoverflow.com/questions/16986331/can-someone-explain-the-e-regex-modifier
